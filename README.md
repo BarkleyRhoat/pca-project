55bcb2bd7ebf41a94e02b3d4caf369ad8a431f77
Barkley Rhoat <barkleyrhoat@gmail.com>
2026-02-24 22:49:02 -0500

    added a mouse trail effect and a language rotation effect on the home screen. Chenged the font and linked google fonts to the index.html

--- README.md
+++ README.md
@@ -5,26 +5,28 @@
   * [Description](#description)
   * [Requirements](#requirements)
   * [Usage](#usage)
-  * [Contact-Me](#contact-me)
   * [Contributors](#contributors)
+  * [Resources](#resources)
   * [Testing](#testing)
   * [License](#license)
   ## About Project 
   ![screenshot]
 ### Built with
 This project was built with 
-* 
-* 
-* 
-* 
+* JavaScript
+* HTML
+* CSS
+* Google Fonts
 * 
 
 
 ## Usage
 
 ## Contributors
-* * [MDN Web Docs - Animation](https://developer.mozilla.org/en-US/docs/Web/API/Window/requestAnimationFrame)
-* 
+
+## Resources
+*  [MDN Web Docs - Animation](https://developer.mozilla.org/en-US/docs/Web/API/Window/requestAnimationFrame)
+* [Mouse Trail Effect Tutorial](https://www.youtube.com/watch?v=ZotButrBGVI)
 * 
   
 ## Testing
--- index.js
+++ index.js
@@ -1,4 +1,13 @@
+// circle animation for h1 element
 const h1 = document.querySelector('h1');
+// fun little language rotation effect
+const languages = ['Welcome', 'Bem-vindo', 'Bienvenido', 'Bienvenue', 'ようこそ', '欢迎'];
+let currentLanguageIndex = 0;
+
+setInterval(() => {
+    currentLanguageIndex = (currentLanguageIndex + 1) % languages.length;
+    h1.textContent = languages[currentLanguageIndex];
+}, 2000);
 let angle = 0;
 const radius = 50;
 h1.style.position = 'absolute';
@@ -19,3 +28,14 @@ function animate() {
 }
 
 animate();
+
+// mouse trail effect 
+document.addEventListener('mousemove', (e) => {
+    const trail = document.createElement('div');
+    trail.className = 'trail';
+    trail.style.left = e.pageX + 'px';
+    trail.style.top = e.pageY + 'px';
+    document.body.appendChild(trail);
+    
+    setTimeout(() => trail.remove(), 500);
+});
